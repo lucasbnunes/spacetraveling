eea3fe46c6c334e3b2bb88b6da054548cba0556c
lucasbnunes <lucasbnunes23@gmail.com>
2021-11-26 15:36:48 -0300

    create home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,9 +7,9 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
-          ></link>
+          />
           <link rel="shortcut icon" href="/images/favicon.ico" />
         </Head>
         <body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,9 +27,71 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <div className={commonStyles.container}>
+      <img src="/images/logo.png" className={styles.logo} />
+
+      <ul>
+        <li className={styles.post}>
+          <Link href="/" passHref>
+            <a>
+              <h2>Título do post</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+              <div className={styles.postInfo}>
+                <span>
+                  <FiCalendar /> 19 Abr 2021
+                </span>
+                <span>
+                  <FiUser /> John Doe
+                </span>
+              </div>
+            </a>
+          </Link>
+        </li>
+
+        <li className={styles.post}>
+          <Link href="/" passHref>
+            <a>
+              <h2>Título do post</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+              <div className={styles.postInfo}>
+                <span>
+                  <FiCalendar /> 19 Abr 2021
+                </span>
+                <span>
+                  <FiUser /> John Doe
+                </span>
+              </div>
+            </a>
+          </Link>
+        </li>
+
+        <li className={styles.post}>
+          <Link href="/" passHref>
+            <a>
+              <h2>Título do post</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+              <div className={styles.postInfo}>
+                <span>
+                  <FiCalendar /> 19 Abr 2021
+                </span>
+                <span>
+                  <FiUser /> John Doe
+                </span>
+              </div>
+            </a>
+          </Link>
+        </li>
+      </ul>
+
+      <button className={styles.button}>Carregar mais posts</button>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
