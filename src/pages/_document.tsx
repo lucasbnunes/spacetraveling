e55c33b14d94755940d441c7afa05f0be2bcb7a7
lucasbnunes <lucasbnunes23@gmail.com>
2021-11-26 14:18:12 -0300

    create document head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,22 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
