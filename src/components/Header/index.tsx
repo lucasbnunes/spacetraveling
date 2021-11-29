a80bfb55e90df6fc74cb3fe7271ffc13e6673ccf
lucasbnunes <lucasbnunes23@gmail.com>
2021-11-29 14:54:21 -0300

    create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,8 @@
+import styles from './header.module.scss';
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.header}>
+      <img src="/images/logo.png" alt="spacetraveling" />
+    </header>
+  );
 }
